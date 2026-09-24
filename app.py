a545e27fb8a81dc0cc6d34ae747bb5a26c0374f1
lekhya <lekhya.gopireddy@gmail.com>
2026-09-24 13:48:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('register.html')
+
 @app.route('/register', methods=['POST'])
 def register():
     name = request.form['name']
@@ -10,7 +13,13 @@ def register():
     email = request.form['email']
     year = request.form['year']
 
-    return render_template('success.html', name=name, student_id=student_id, email=email, year=year)
+    return render_template(
+        'success.html',
+        name=name,
+        student_id=student_id,
+        email=email,
+        year=year
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
